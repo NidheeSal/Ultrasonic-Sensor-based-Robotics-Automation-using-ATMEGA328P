5c3d2d43026ef8523dc29d2772542347b4398309
NidheeSal <108951540+NidheeSal@users.noreply.github.com>
2025-12-04 12:09:18 +0100

    Add files via upload

---
+++ Obstacle_Avoiding_Robot.ino
@@ -0,0 +1,126 @@
+#include <NewPing.h>
+#include <AFMotor.h>
+#include <Servo.h>
+
+#define echopin A0 // echo pin
+#define trigpin A5 // Trigger pin
+
+Servo myservo;
+
+const int MOTOR_1 = 1; //const defined because value should not change
+const int MOTOR_2 = 2; 
+//const int MOTOR_3 = 3; 
+//const int MOTOR_4 = 4; 
+
+AF_DCMotor motor1(MOTOR_1, MOTOR12_64KHZ); // create motor object, 64KHz pwm (AF_DCMotor motorname(portnum, freq))
+AF_DCMotor motor2(MOTOR_2, MOTOR12_64KHZ); // create motor object, 64KHz pwm
+//AF_DCMotor motor3(MOTOR_3, MOTOR12_64KHZ); // create motor object, 64KHz pwm
+//AF_DCMotor motor4(MOTOR_4, MOTOR12_64KHZ); // create motor object, 64KHz pwm
+//===============================================================================
+//  Initialization
+//===============================================================================
+
+int distance_L, distance_F, distance_R;
+long distance;
+
+int set = 10;
+ 
+void setup() {
+  Serial.begin(9600);           // Initialize serial port
+  Serial.println("Start");
+ 
+
+  myservo.attach(10);  //servo pin
+  myservo.write(90);  //Servo position
+
+  pinMode (trigpin, OUTPUT);    // set trig pin as output
+  pinMode (echopin, INPUT);     //set echo pin as input to capture reflected waves
+  
+  motor1.setSpeed(255);         // set the motor speed to 0-255 (Set motor 1 to maximum speed)
+  motor2.setSpeed(255);         //Set motor 2 to maximum speed
+  //motor3.setSpeed(180);
+//  motor4.setSpeed(180);
+}
+//===============================================================================
+//  Main
+//=============================================================================== 
+void loop() {
+ distance_F = data();
+ Serial.print("S=");
+ Serial.println(distance_F);
+ delay(1000);
+  if (distance_F > set){
+   Serial.println("Forward");
+  motor1.run(FORWARD);         // turn it on going forward
+  motor2.run(FORWARD); 
+//  motor3.run(FORWARD); 
+//  motor4.run(FORWARD);
+    }
+    else{hc_sr4();
+    }
+}
+
+
+long data(){
+  //Write a pulse to the HC-SR05 Trigger pin
+  //Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  digitalWrite(trigpin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigpin, HIGH);
+  delayMicroseconds(10);
+  //Measure the response from the HC-SR05 echo pin
+  distance = pulseIn (echopin, HIGH);
+  return (distance /2)/29; // Divide by 29 or multiply by 0.0343
+}
+
+
+void compareDistance(){
+  if (distance_L > distance_R){
+  motor1.run(FORWARD);   // turn it on going left
+  motor2.run(BACKWARD);
+//  motor3.run(FORWARD); 
+//  motor4.run(FORWARD); 
+    delay(350);
+  }
+  else if (distance_R > distance_L){
+  motor1.run(BACKWARD);  // the other right
+  motor2.run(FORWARD); 
+//  motor3.run(BACKWARD); 
+//  motor4.run(BACKWARD);
+    delay(350);
+  }
+  else{
+  motor1.run(BACKWARD);  // back
+  motor2.run(BACKWARD);
+//  motor3.run(BACKWARD); 
+//  motor4.run(BACKWARD); 
+   delay(300);
+  motor1.run(FORWARD);   // turn it on going left
+  motor2.run(BACKWARD);
+//  motor3.run(FORWARD); 
+//  motor4.run(FORWARD); 
+    delay(500);
+  }
+}
+
+void hc_sr4(){
+    Serial.println("Stop");
+    motor1.run(RELEASE);         // stopped
+    motor2.run(RELEASE);
+
+    
+//    motor3.run(RELEASE);
+//    motor4.run(RELEASE);
+   
+    myservo.write(0);
+    delay(300);
+    distance_R = data();
+    delay(100);
+    myservo.write(170);
+    delay(500);
+    distance_L = data();
+    delay(100);
+    myservo.write(90);
+    delay(300);
+    compareDistance();
+}
